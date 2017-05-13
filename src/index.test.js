ac6a5036c63c383a39999ac80c1f38caabb5a437
priyank1205 <priyank.agarwal1205@gmail.com>
2017-05-13 23:22:14 +0530

    Initial commit

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+import {
+  expect
+} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('index.html', () => {
+  it('should say Hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello, world! This is Priyank Agarwal");
+      done();
+      window.close();
+    })
+  })
+});
