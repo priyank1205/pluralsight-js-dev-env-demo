1b738e193a51cd2a6aa8fabd4ac507628d4794a5
priyank1205 <priyank.agarwal1205@gmail.com>
2017-05-14 15:45:59 +0530

    API Module completed

--- src/index.test.js
+++ src/index.test.js
@@ -5,11 +5,11 @@ import jsdom from 'jsdom';
 import fs from 'fs';
 
 describe('index.html', () => {
-  it('should say Hello', (done) => {
+  it('should have h1 saying Users', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello, world! This is Priyank Agarwal");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     })
